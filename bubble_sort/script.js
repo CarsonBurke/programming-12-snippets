999e084d8433757d671179829c226e29cbbbb7eb
Carson <carsonburke22@gmail.com>
2023-02-27 11:24:37 -0800

    completed bubble sort

---
+++ bubble_sort/script.js
@@ -0,0 +1,44 @@
+/* 
+How to use:
+    "node bubble_sort/script.js" while in the repo folder
+*/
+
+function bubbleSort(array) {
+
+    const lastIndex = array.length - 1
+    let totalIterations = 0
+
+    for (let i = 0; i < lastIndex; i++) {
+
+        console.log(`Pass ${i + 1}:`)
+        let swap
+
+        for (let j = 0; j < lastIndex - i; j++) {
+            totalIterations += 1
+
+            const val1 = array[j],
+            val2 = array[j + 1]
+
+            if (val1 < val2) continue
+            console.log(`   Swapped ${val1} at ${j} with ${val2} at ${j + 1}`)
+            // i1 is greater than i2, swap
+
+            array[j] = val2
+            array[j + 1] = val1
+            swap = true
+        }
+
+        if (!swap) {
+
+            console.log('   No swap, stopping')
+            break
+        }
+    }
+
+    console.log('Result:')
+    console.log('   Total iterations', totalIterations)
+    console.log('   Slowest possible', array.length * (lastIndex) / 2)
+    console.log('   Sorted array', array)
+}
+
+bubbleSort([6, 2, 1, 8, -10, 6, 100, 4, -4])
